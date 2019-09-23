00c692987024b88abe374fc35ba52c68d6150b50
Paolo Pironi <ppiron@gmail.com>
2019-09-23 14:18:07 +0100

    part4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,15 @@ const totalLikes = blogs => {
   }, 0)
 }
 
+const favoriteBlog = blogs => {
+  const mostLikes = Math.max(...blogs.map(blog => blog.likes))
+  const blogWithMostLikes = blogs.filter(blog => blog.likes === mostLikes)[0]
+  const { _id, url, __v, ...res } = blogWithMostLikes
+  return res
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
