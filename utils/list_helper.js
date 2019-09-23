e37e8c7b6518393cd1a8bfd990f2e3d68452bc9d
Paolo Pironi <ppiron@gmail.com>
2019-09-23 13:37:42 +0100

    part4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,9 +1,14 @@
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
-
+const totalLikes = blogs => {
+  return blogs.reduce((acc, val) => {
+    return acc + val.likes
+  }, 0)
+}
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
